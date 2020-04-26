3bce6f2a1ef0106f9d18435354d1ecb0236df96f
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-26 10:33:28 -0600

    I have added my handle edit movie event and my click save event when users want to edit movie data in edit page

---
+++ src/components/Pages/EditPage/EditPage.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class EditPage extends Component {
+  state = {
+    movieSelection: {
+      title: "",
+      description: "",
+    },
+  };
+
+  //   mount components
+  componentDidMount() {
+    this.props.dispatch({ type: "GET_LIST" });
+  }
+
+  //  User edits will be inputed on event handler to be stored
+  handleEditEvent = (inputEdits) => (event) => {
+    this.setState({
+      movieSelection: {
+        ...this.state.movieSelection,
+        inputEdits: event.target.value,
+      },
+    });
+  };
+
+  //   User edits made will now be updated
+  handleClickSave = (event) => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: "UPDATE_MOVIES",
+      payload: this.state.movieSelection,
+    });
+    this.props.history.push(`/movies/${this.props.params.id}`);
+  };
+}
+
+const mapStoreToProps = (store) => ({ store });
+
+export default connect(mapStoreToProps)(EditPage);
