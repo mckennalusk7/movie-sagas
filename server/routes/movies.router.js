807c19dfa3cbe9d883a69db8160484a580c182e5
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-25 19:14:08 -0600

    added get route to get genre and movie details from the database

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -3,18 +3,32 @@ const pool = require("../modules/pool");
 
 const router = express.Router();
 
-//  GET ROUTE
-router.get("/", (req, res) => {
+//  GET ROUTE to get movie from database
+router.get("/movie", (req, res) => {
   const queryText = `SELECT * FROM "movies"
   ORDER BY "id";`;
 
+  pool
+    .query(queryText, [req.params.id])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("Error completing SELECT movies query", err);
+      res.sendStatus(500);
+    });
+});
+
+// GET ROUTE to get genre and movie details from database
+router.get("/details/:id", (req, res) => {
+  const queryText = `SELECT * FROM movies WHERE id=$1 GROUP BY "movies.id`;
   pool
     .query(queryText)
     .then((result) => {
       res.send(result.rows);
     })
     .catch((err) => {
-      console.log("Error completing SELECT movies query", err);
+      console.log("ERROR in completing SELECT movies query", err);
       res.sendStatus(500);
     });
 });
